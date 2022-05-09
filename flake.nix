ffa1dd449dc68056970e330555c8a586e8f16703
hugosenari <hugosenari@gmail.com>
2022-05-09 20:35:32 -0300

    chore: re index bgfxdotnim

--- flake.nix
+++ flake.nix
@@ -7,14 +7,6 @@
   inputs.flakeNimbleLib.type  = "github";
   inputs.flakeNimbleLib.inputs.nixpkgs.follows = "nixpkgs";
   
-  inputs."bgfxdotnim-master".dir   = "master";
-  inputs."bgfxdotnim-master".owner = "nim-nix-pkgs";
-  inputs."bgfxdotnim-master".ref   = "master";
-  inputs."bgfxdotnim-master".repo  = "bgfxdotnim";
-  inputs."bgfxdotnim-master".type  = "github";
-  inputs."bgfxdotnim-master".inputs.nixpkgs.follows = "nixpkgs";
-  inputs."bgfxdotnim-master".inputs.flakeNimbleLib.follows = "flakeNimbleLib";
-  
   outputs = { self, nixpkgs, flakeNimbleLib, ...}@inputs:
   let 
     lib  = flakeNimbleLib.lib;
